91b6fbe303a1cfc3416273161eac2e69cc26d743
Piotr Gerke <piotrg2003@gmail.com>
2021-04-23 08:54:43 +0200

    slide in work

---
+++ personal-website/js/showWork.js
@@ -0,0 +1,7 @@
+function workAppear(){
+    document.getElementById("my-work").classList.toggle("slide-in");
+}
+
+function workDisappear(){
+    document.getElementById("my-work").classList.remove("slide-in");
+}
